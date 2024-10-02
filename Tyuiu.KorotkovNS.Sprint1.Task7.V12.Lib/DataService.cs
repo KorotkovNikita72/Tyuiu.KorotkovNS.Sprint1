f5446ec12f4fa0ac13bc27c08ed0c3179d12ce9c
tresh78805@gmail.com <tresh78805@gmail.com>
2024-10-02 12:11:37 +0500

    Task7_V12

---
+++ Tyuiu.KorotkovNS.Sprint1.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KorotkovNS.Sprint1.Task7.V12.Lib
+{
+    public class DataService : ISprint1Task7V12
+    {
+        public double Calculate(double x, double y)
+        {
+            double res = Math.Pow((x + 1) / (x - 1), x) + 18 * x * Math.Pow((y), 2);
+            return res;
+        }
+    }
+}
